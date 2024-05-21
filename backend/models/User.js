f384daf9ecee5d6ab12622dddb842a48dd23d34e
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-21 20:29:24 +0530

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
