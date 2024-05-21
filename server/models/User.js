20ed70a071e9edc148dcd000bf8e60324b988004
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-21 22:19:11 +0530

    model updated
    Co-Authored-By: Nadee Tharuka <121155318+nadeetharuka@users.noreply.github.com>
    Co-Authored-By: Isuru Dayananda <129027427+isurudayananda@users.noreply.github.com>
    Co-Authored-By: UdariAdhikaram <121155113+udariadhikaram@users.noreply.github.com>

--- server/models/User.js
+++ server/models/User.js
@@ -3,13 +3,49 @@ const sequelize = require('../db');
 
 const User = sequelize.define('User', {
   username: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(15),
+    allowNull: false,
+    primaryKey: true,
+  },
+  hashedPassword: {
+    type: DataTypes.STRING(64),
     allowNull: false,
   },
-  password: {
-    type: DataTypes.STRING,
+  email: {
+    type: DataTypes.STRING(20),
     allowNull: false,
   },
+  verified: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  firstName: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+  },
+  dob: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  bio: {
+    type: DataTypes.STRING(150),
+  },
+  profilePicture: {
+    type: DataTypes.BLOB,
+  },
+  postsCount: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+}, {
+  tableName: 'user',
+  timestamps: false,
+  engine: 'MYISAM',
+  charset: 'latin1',
 });
 
 module.exports = User;
