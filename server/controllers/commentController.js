4d381c3fb5b4ea3e37a46e4e63c21d931c9dc3d5
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-23 19:34:33 +0530

    bugfix: comment route

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -9,6 +9,88 @@ const {Op} = require("sequelize");
 const router = express.Router();
 const upload = multer();
 
+router.get('/', async (req, res) => {
+    const { post } = req.query;
 
+    if (!post) {
+        return res.status(400).send('Post id is required');
+    }
+
+    const postExists = await Post.findByPk(post);
+
+    if (!postExists) {
+        return res.status(404).send('Post not found');
+    }
+
+    const comments = await Comment.findAll({ where: { post } });
+
+    res.json(comments);
+});
+
+router.post('/', upload.none(), authMiddleware, async (req, res) => {
+    const { post, text } = req.body;
+    const user = req.user.username;
+
+    if (!user || !post) {
+        return res.status(400).send('Post id is required and you must be logged in');
+    }
+
+    const postExists = await Post.findByPk(post);
+
+    if (!postExists) {
+        return res.status(404).send('Post not found');
+    }
+
+    const comment = await Comment.create({
+        post,
+        user,
+        text,
+        created: new Date(),
+        last_modified: new Date()
+    });
+
+    res.status(201).send('Comment added successfully');
+});
+
+router.put('/', upload.none(), authMiddleware, async (req, res) => {
+    const { id, text } = req.body;
+    const user = req.user.username;
+
+    if (!user || !id) {
+        return res.status(400).send('Comment id is required and you must be logged in');
+    }
+
+    const comment = await Comment.findOne({ where: { id, user } });
+
+    if (!comment) {
+        return res.status(404).send('Comment not found');
+    }
+
+    comment.text = text;
+    comment.last_modified = new Date();
+
+    await comment.save();
+
+    res.status(200).send('Comment updated successfully');
+});
+
+router.delete('/', upload.none(), authMiddleware, async (req, res) => {
+    const { id } = req.body;
+    const user = req.user.username;
+
+    if (!user || !id) {
+        return res.status(400).send('Comment id is required and you must be logged in');
+    }
+
+    const comment = await Comment.findOne({ where: { id, user } });
+
+    if (!comment) {
+        return res.status(404).send('Comment not found');
+    }
+
+    await comment.destroy();
+
+    res.status(200).send('Comment deleted successfully');
+});
 
 module.exports = router;
