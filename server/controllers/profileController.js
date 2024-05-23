ea7b5bff73de2219f373497788ec1e04bfb4e6e4
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-23 21:36:14 +0530

    get user profile route

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -8,6 +8,20 @@ const {Op} = require("sequelize");
 const router = express.Router();
 const upload = multer();
 
+router.get('/', upload.none(), authMiddleware, async (req, res) => {
+  const { username } = req.body;
 
+  if (!username) {
+    return res.status(400).send('Username is required');
+  }
+
+  const user = await User.findOne({ where: { username } });
+
+  if (!user) {
+    return res.status(404).send('User not found');
+  }
+
+  res.json(user);
+});
 
 module.exports = router;
