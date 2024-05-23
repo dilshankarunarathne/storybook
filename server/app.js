c8121c67590306ed4a333d5ce7b678a8cf9eae0d
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-23 19:07:18 +0530

    created the reaction router

--- server/app.js
+++ server/app.js
@@ -13,11 +13,13 @@ db.sync()
 
 const authController = require('./controllers/authController');
 const homeController = require('./controllers/homeController');
+const reactController = require('./controllers/reactController');
 
 app.use(cors()); 
 app.use(express.json());
 
 app.use('/auth', authController);
 app.use('/', homeController);
+app.use('/react', reactController);
 
 app.listen(8000, () => console.log('Server started on port 8000'));
