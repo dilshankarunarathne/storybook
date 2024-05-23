ac9d12a80d1daee8d93f3c476bc3323a8d3a1e68
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-23 17:40:56 +0530

    Create mailer.js

---
+++ server/middleware/mailer.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const Mailgen = require('mailgen');
+require('dotenv').config();
+const app = express();
+const port = 3001;
+
+let config = {
+    service: 'gmail',
+    auth: {
+        user: process.env.NODEJS_GMAIL_APP_USER,
+        pass: process.env.NODEJS_GMAIL_APP_PASSWORD
+    }
+}
+
+let transporter = nodemailer.createTransport(config);
+
+function sendEmail(to, subject, html, attachments = []) {
+    let message = {
+        from: 'dimankaperera@gmail.com', 
+        to: to, 
+        subject: subject, 
+        html: html, 
+        attachments: attachments
+    };
+
+    transporter.sendMail(message).then((info) => {
+        console.log("Email sent", info.messageId, nodemailer.getTestMessageUrl(info));
+    }).catch((err) => {
+        console.error(err);
+    });
+}
+
+// sendEmail('user.ftp.server@gmail.com', 'You have attended', '<b>You have attended to class...</b>')
+
+module.exports = sendEmail;
