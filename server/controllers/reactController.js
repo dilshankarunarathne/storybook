e69e5df6d35fa1806765a53bd8760fce1ea23d56
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-23 19:12:49 +0530

    add, delete reaction endpoints

--- server/controllers/reactController.js
+++ server/controllers/reactController.js
@@ -8,6 +8,39 @@ const {Op} = require("sequelize");
 const router = express.Router();
 const upload = multer();
 
+router.post('/', upload.none(), authMiddleware, async (req, res) => {
+    const { post } = req.body;
+    const user = req.user.username;
 
+    if (!user || !post) {
+        return res.status(400).send('Post id is required and you must be logged in');
+    }
+
+    const react = await React.create({
+        user,
+        post
+    });
+
+    res.status(201).send('React added successfully');
+});
+
+router.delete('/', upload.none(), authMiddleware, async (req, res) => {
+    const { post } = req.body;
+    const user = req.user.username;
+
+    if (!user || !post) {
+        return res.status(400).send('Post id is required and you must be logged in');
+    }
+
+    const react = await React.findOne({ where: { user, post } });
+
+    if (!react) {
+        return res.status(404).send('React not found');
+    }
+
+    await react.destroy();
+
+    res.status(200).send('React deleted successfully');
+});
 
 module.exports = router;
