1daa28680a25e3942c89d74893dacebcdc02c60e
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-23 18:43:31 +0530

    delete, update post endpoints

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -8,6 +8,22 @@ const {Op} = require("sequelize");
 const router = express.Router();
 const upload = multer();
 
+router.post('/', authMiddleware, async (req, res) => {
+    const username = req.user.username;
+
+    const posts = await Post.findAll({
+        where: {
+            user: {
+                [Op.ne]: username
+            }
+        },
+        order: [
+            ['created', 'DESC']
+        ]
+    });
+    res.json(posts);
+});
+
 router.post('/post', upload.none(), authMiddleware, async (req, res) => {
     const { text, image } = req.body;
 
@@ -26,20 +42,50 @@ router.post('/post', upload.none(), authMiddleware, async (req, res) => {
     res.status(201).send('Post created successfully');
 });
 
-router.post('/', authMiddleware, async (req, res) => {
-    const username = req.user.username;
+router.put('/post', upload.none(), authMiddleware, async (req, res) => {
+    const { post_id, text, image } = req.body;
 
-    const posts = await Post.findAll({
-        where: {
-            user: {
-                [Op.ne]: username
-            }
-        },
-        order: [
-            ['created', 'DESC']
-        ]
-    });
-    res.json(posts);
+    if (!post_id) {
+        return res.status(400).send('Post id is required');
+    }
+
+    const post = await Post.findByPk(post_id);
+
+    if (!post) {
+        return res.status(404).send('Post not found');
+    }
+
+    if (text) {
+        post.text = text;
+    }
+
+    if (image) {
+        post.image = image;
+    }
+
+    post.last_modified = new Date();
+
+    await post.save();
+
+    res.status(200).send('Post updated successfully');
+});
+
+router.delete('/post', upload.none(), authMiddleware, async (req, res) => {
+    const { post_id } = req.body;
+
+    if (!post_id) {
+        return res.status(400).send('Post id is required');
+    }
+
+    const post = await Post.findByPk(post_id);
+
+    if (!post) {
+        return res.status(404).send('Post not found');
+    }
+
+    await post.destroy();
+
+    res.status(200).send('Post deleted successfully');
 });
 
 module.exports = router;
