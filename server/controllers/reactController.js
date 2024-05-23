2954750046759ebd6c972f1baa1adb7594a9056c
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-23 19:18:05 +0530

    bugfix: non-existing posts reactable

--- server/controllers/reactController.js
+++ server/controllers/reactController.js
@@ -1,5 +1,6 @@
 const authMiddleware = require('../middleware/authMiddleware');
 const React = require('../models/React');
+const Post = require('../models/Post');
 
 const multer = require('multer');
 const express = require('express');
@@ -14,6 +15,12 @@ router.post('/', upload.none(), authMiddleware, async (req, res) => {
 
     if (!user || !post) {
         return res.status(400).send('Post id is required and you must be logged in');
+    }
+
+    const postExists = await Post.findByPk(post);
+
+    if (!postExists) {
+        return res.status(404).send('Post not found');
     }
 
     const react = await React.create({
