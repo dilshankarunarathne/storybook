c2cb8aa7f1679cf2d308073cf77b50fdf4ad13bc
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-23 21:25:54 +0530

    forgot password feature

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -68,4 +68,48 @@ router.post('/verify', upload.none(), async (req, res) => {
   res.status(200).send('User confirmed successfully');
 });
 
+router.get('/forgot', upload.none(), async (req, res) => {
+  const { username } = req.body;
+
+  if (!username) {
+    return res.status(400).send('Username is required');
+  }
+
+  const user = await User.findOne({ where: { username } });
+
+  if (!user) {
+    return res.status(404).send('User not found');
+  }
+
+  user.verificationCode = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+
+  await user.save();
+
+  // TODO: Send verification code to user via email
+
+  res.status(200).send('Password reset code sent successfully');
+});
+
+router.post('/forgot', upload.none(), async (req, res) => {
+  const { password } = req.body;
+  const code = req.query.code;
+
+  if (!password || !code) {
+    return res.status(400).send('Password and verification code are required');
+  }
+
+  const user = await User.findOne({ where: { verificationCode: code } });
+
+  if (!user) {
+    return res.status(404).send('Invalid verification code');
+  }
+
+  user.hashed_password = await bcrypt.hash(password, 10);
+  user.verificationCode = null;
+
+  await user.save();
+
+  res.status(200).send('Password reset successfully');
+});
+
 module.exports = router;
