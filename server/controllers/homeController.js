d15db3209986b627828212dcf7acc0b6276ed11f
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-23 18:13:06 +0530

    Update homeController.js

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,11 +1,23 @@
 const authMiddleware = require('../middleware/authMiddleware');
+const Post = require('../models/Post');
 
 const express = require('express');
 
 const router = express.Router();
 
-router.get('/', authMiddleware, (req, res) => {
-    res.send('This is home');
+router.get('/', authMiddleware, async (req, res) => {
+    const username = req.user.username;
+    const posts = await Post.findAll({
+        where: {
+            user: {
+                [Op.ne]: username
+            }
+        },
+        order: [
+            ['created', 'DESC']
+        ]
+    });
+    res.json(posts);
 });
 
 module.exports = router;
