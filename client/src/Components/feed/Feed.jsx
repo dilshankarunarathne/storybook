e07b7c6cf8e1e66bbe74b46ae462908a3258d93c
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-05-25 03:28:31 +0530

    feed connect share and posts

---
+++ client/src/Components/feed/Feed.jsx
@@ -0,0 +1,23 @@
+import Post from "../post/Post"
+import Share from "../share/Share"
+import "./feed.css"
+
+export default function Feed() {
+  return (
+    <div className="feed">
+        <div className="feedWrapper">
+          <Share/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+        </div>
+    </div>
+  )
+}
