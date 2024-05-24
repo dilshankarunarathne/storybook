2cffb6a00976204e146209febf1d8062d1f09e85
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-24 20:28:12 +0530

    bugfix: get post exchange

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -79,7 +79,7 @@ router.get('/verify', upload.none(), async (req, res) => {
   res.status(200).send('User confirmed successfully');
 });
 
-router.get('/forgot', upload.none(), async (req, res) => {
+router.post('/forgot', upload.none(), async (req, res) => {
   const { username } = req.body;
 
   if (!username) {
@@ -109,7 +109,7 @@ router.get('/forgot', upload.none(), async (req, res) => {
   res.status(200).send('Password reset code sent successfully');
 });
 
-router.post('/forgot', upload.none(), async (req, res) => {
+router.get('/forgot', upload.none(), async (req, res) => {
   const { password } = req.body;
   const code = req.query.code;
 
