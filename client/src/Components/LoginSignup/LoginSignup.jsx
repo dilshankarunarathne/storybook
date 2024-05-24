c90d48088cce68ba4b1195f9a5693d2d64da9a03
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-05-24 20:12:44 +0530

    update login page

--- client/src/Components/LoginSignup/LoginSignup.jsx
+++ client/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginSignup.css'
-import { Link } from 'react-router-dom'
 
-//import user_icon from '../Assests/person.png'
-//import email_icon from '../Assests/email.png'
-//import password_icon from '../Assests/password.png'
+import user_icon from '../Assests/person.png'
+import email_icon from '../Assests/email.png'
+import password_icon from '../Assests/password.png'
 
 const LoginSignup = () => {
+
+  const [action,setAction] = useState("Sign Up");
+
   return (
     <div className='container'>
-      <h1>Login form</h1>
-      <button><Link to="./HeaderArea/HeaderArea">LogIn</Link>
-      </button>
-
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+        <div className="inputs">
+          {action==="Login"?<div></div>:<div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder="Name" />
+          </div>}
+          
+          <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" placeholder="Email ID" />
+          </div>
+          <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" placeholder="Password"/>   
+          </div>
+        </div>
+        {action==="Sign Up"?<div></div>: <div className="forgot-password">Lost Password? <span>Click Here</span></div>}
+       
+        <div className="submit-container">
+          <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+          <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+        </div>
     </div>
   )
 }
