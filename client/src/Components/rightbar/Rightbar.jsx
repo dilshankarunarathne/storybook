52f81dcf09f59c259132b3e327891f96ae9e763e
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-05-25 03:30:33 +0530

    create rightbar

---
+++ client/src/Components/rightbar/Rightbar.jsx
@@ -0,0 +1,16 @@
+import "./rightbar.css"
+
+export default function Rightbar() {
+  return (
+    <div className="rightbar">
+        <div className="rightbarWrapper">
+          <div className="birthdayContainer">
+            <img src="assets/gift.jpg" alt="" className="birthdayImg"/>
+            <span className="birthdayText">
+              <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today
+            </span>
+          </div>
+        </div>
+    </div>
+  )
+}
