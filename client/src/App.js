69f6cb6acc6d64462d4ed9760b1595461905ffb6
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-05-24 19:33:19 +0530

    update login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 //import logo from './logo.svg';
 import './App.css';
+import { Home } from './Home/Home';
 //import LoginSignup from './Components/LoginSignup/LoginSignup';
 //import { Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 //import HeaderArea from './Components/HeaderArea/HeaderArea';
 
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className='App'>
+      <Home/>
+
     </div>
   );
 }
