f2c2c09ab1c456bb95377210ec11b0397cd8a742
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-05-25 22:51:03 +0530

    changing register component

--- client/src/Components/register/Register.jsx
+++ client/src/Components/register/Register.jsx
@@ -2,31 +2,32 @@ import "./register.css"
 
 export default function Register() {
   return (
-    <div className="login">
-        <div className="loginWrapper">
-            <div className="loginLeft">
-                <h3 className="loginLogo">StoryBook</h3>
-                <span className="loginDesc">
+    <div className="register">
+        <div className="registerWrapper">
+            <div className="registerLeft">
+                <h3 className="registerLogo">StoryBook</h3>
+                <span className="registerDesc">
                     Connect with friends and 
                     the world around you on StoryBook.
                 </span>
             </div>
-            <div className="loginRight">
-                <div className="loginBox">
-                    <input placeholder="Username" className="loginInput" />
-                    <input placeholder="Email" className="loginInput" />
-                    <input placeholder="Password" className="loginInput" />
+            <div className="registerRight">
+                <div className="registerBox">
+                    <input type="text" placeholder="Username" className="registerInput" />
+                    <input type="email" placeholder="Email" className="registerInput" />
+                    <input type="password" placeholder="Password" className="registerInput" />
+                    <input type="password" placeholder="Confirm Password" className="registerInput" />
                     <div className="nameContainer">
                         <div className="fnameLeft">
-                            <input placeholder="Firstname" className="loginInput" />
+                            <input placeholder="Firstname" className="registerInput" />
                         </div>
                         <div className="lnameRight">
-                            <input placeholder="Lastname" className="loginInput" />
+                            <input placeholder="Lastname" className="registerInput" />
                         </div>
                         </div>
-                    <input placeholder="Date of Birth" className="loginInput" />
-                    <button className="loginButton">Sign Up</button>
-                    <button className="loginRegisterButton">Log into Account</button>
+                    <input placeholder="Date of Birth" className="registerInput" />
+                    <button className="registerButton">Sign Up</button>
+                    <button className="logRegisterButton">Log into Account</button>
                 </div>
             </div>
         </div>
