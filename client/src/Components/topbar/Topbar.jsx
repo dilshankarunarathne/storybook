780a438ce6ea53a062ec04ca4dcb60e4c45bbfbf
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-05-25 16:35:16 +0530

    changing some codes

--- client/src/Components/topbar/Topbar.jsx
+++ client/src/Components/topbar/Topbar.jsx
@@ -1,38 +1,16 @@
 import "./topbar.css"
-import { Search , Person, Chat, Notifications} from '@mui/icons-material';
 
 export default function Topbar() {
   return (
     <div className='topbarContainer'>
         <div className="topbarLeft">
-            <span className="logo">MetaSocial</span>
-        </div>
-
-        <div className="topbarCenter">
-            <div className="searchbar">
-                <Search className="searchIcon"/>
-                <input placeholder="Search for friends, post or video" className="searchInput" />
-            </div>
+            <span className="logo">StoryBook</span>
         </div>
 
         <div className="topbarRight">
             <div className="topbarLinks">
                 <span className="topbarLink">Homepage</span>
-                <span className="topbarLink">Timeline</span>
-            </div>
-            <div className="topbarIcons">
-                <div className="topbarIconItem">
-                    <Person/>
-                    <span className="topbarIconBadge">1</span>
-                </div>
-                <div className="topbarIconItem">
-                    <Chat/>
-                    <span className="topbarIconBadge">2</span>
-                </div>
-                <div className="topbarIconItem">
-                    <Notifications/>
-                    <span className="topbarIconBadge">1</span>
-                </div>
+                <span className="topbarLink">Logout</span>
             </div>
             <img src="/assets/4.jpg" alt="" className="topbarImg"/>
         </div>
