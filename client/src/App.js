62e3fb52a088dae34a4763330747851b2ed01c69
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-05-25 22:51:32 +0530

    mount register in app

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/home/Home';
 import AuthProvider from './context/AuthProvider';
 import Login from './Components/login/Login';
+import Register from './Components/register/Register';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Switch>
-          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
           <Route path="/" component={Home} />
         </Switch>
       </Router>
