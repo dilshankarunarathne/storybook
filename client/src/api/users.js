ef6b706b9f884449273e4f2a7f203407faaef1d8
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-26 10:38:01 +0530

    removed debug code

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,7 +1,6 @@
 const API_URL = 'http://localhost:8000/auth';
 
 export const loginUser = async (username, password) => {
-    console.log('called1');
     const formData = new FormData();
     formData.append('username', username);
     formData.append('password', password);
