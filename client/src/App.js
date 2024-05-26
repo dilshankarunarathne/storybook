ad605c18e11c9a44f23c0e19ab041f4f4f916e51
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-26 10:01:46 +0530

    routed profile

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import Home from './pages/home/Home';
 import AuthProvider from './context/AuthProvider';
 import Register from './Components/register/Register';
 import Login from "./Components/login/Login";
+import Profile from "./Components/profile/Profile";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Switch>
-          <Route path="/register" component={Register} />
             <Route path="/" component={Home} />
+            <Route path="/register" component={Register} />
             <Route path="/login" component={Login} />
+            <Route path="/profile" component={Profile} />
         </Switch>
       </Router>
     </AuthProvider>
