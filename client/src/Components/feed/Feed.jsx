a8688c6086f55ad8952700218ea70da4766fdd24
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-26 15:26:46 +0530

    wall integrated

--- client/src/Components/feed/Feed.jsx
+++ client/src/Components/feed/Feed.jsx
@@ -1,24 +1,36 @@
+import { useEffect, useState } from 'react';
+
 import Post from "../post/Post"
 import Share from "../share/Share"
 
 import "./feed.css"
 
 export default function Feed() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const token = localStorage.getItem('token');
+            const response = await fetch('http://localhost:8000/', {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setPosts(data);
+            }
+        };
+
+        fetchPosts();
+    }, []);
+
     return (
         <div className="feed">
             <div className="feedWrapper middle">
                 <Share/>
-
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                {posts.map(post => <Post key={post.post_id} post={post} />)}
             </div>
         </div>
     )
--- client/src/Components/post/Post.jsx
+++ client/src/Components/post/Post.jsx
@@ -2,34 +2,31 @@ import {MoreVert} from "@mui/icons-material"
 
 import "./post.css"
 
-export default function Post() {
+export default function Post({ post }) {
     return (
         <div className="post">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
                         <img className="postProfileImg" src="/assets/feed1.jpg" alt=""/>
-                        <span className="postUsername">Daisy Audrey</span>
-                        <span className="postDate">5 mins ago</span>
+                        <span className="postUsername">{post.user}</span>
+                        <span className="postDate">{new Date(post.created).toDateString()}</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVert/>
                     </div>
                 </div>
                 <div className="postCenter">
-                    <span className="postText">Hey! Its my first post:) </span>
-                    <img className="postImg" src="assets/post1.jpg" alt=""/>
+                    <span className="postText">{post.text}</span>
+                    {post.image && <img className="postImg" src={post.image} alt=""/>}
                 </div>
-
-                {/*TODO: change the style of buttons */}
                 <div className="postBottom">
                     <div className="postBottomLeft">
                         <img className="likeIcon" src="assets/like1.jpg" alt=""/>
-                        <img className="likeIcon" src="assets/hrt.jpg" alt=""/>
-                        <span className="postlikeCounter">32 People like it</span>
+                        <span className="postlikeCounter">{post.likes_count} People like it</span>
                     </div>
                     <div className="postBottomRight">
-                        <span className="postCommentText">8 comments</span>
+                        <span className="postCommentText">{post.comments_count} comments</span>
                     </div>
                 </div>
             </div>
