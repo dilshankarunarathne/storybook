5f2f50d96b01412d2598906d3b1dcfa0315462b6
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-27 17:41:31 +0530

    swagger setup

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const app = express();
 
 const db = require('./db');
@@ -10,6 +13,23 @@ const db = require('./db');
 db.sync()
   .then(() => console.log('Tables have been created'))
   .catch(error => console.error('Unable to create tables', error));
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'StoryBook API',
+      version: '1.0.0',
+      description: 'API Documentation for StoryBook',
+    },
+    servers: [
+      {
+        url: 'http://localhost:8000',
+      },
+    ],
+  },
+  apis: ['./controllers/*.js'],
+};
 
 const authController = require('./controllers/authController');
 const homeController = require('./controllers/homeController');
@@ -25,5 +45,8 @@ app.use('/auth', authController);
 app.use('/react', reactController);
 app.use('/comment', commentController);
 app.use('/profile', profileController);
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(8000, () => console.log('Server started on port 8000'));
