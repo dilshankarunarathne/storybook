0e226ad7ff9e515dab70240b65226510358829cd
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-05-27 18:01:18 +0530

    configured swagger to write api.json

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const fs = require('fs');
+
 const express = require('express');
 const cors = require('cors');
 
@@ -48,5 +50,6 @@ app.use('/profile', profileController);
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+fs.writeFileSync('./swagger.json', JSON.stringify(swaggerDocs, null, 2));
 
 app.listen(8000, () => console.log('Server started on port 8000'));
